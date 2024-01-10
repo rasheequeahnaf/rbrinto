208eb7730613595fc0c8273ee10494f84e207a8c
Rasheeque Ahnaf Brinto <rbrinto@myseneca.ca>
2024-01-10 16:30:58 -0500

    Added lab1a

--- lab1a.py
+++ lab1a.py
@@ -1,2 +1,16 @@
-#!/usr/bin/python3
-print("Hello, World!")
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc). 
+
+print('Hello world')
+
+
+
+'''#!/usr/bin/env python3
+print('Hello world\n')
+'''
